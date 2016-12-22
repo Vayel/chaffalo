8515057310baf1b3190a4e5b7f7fe53c491dc9b4
Vincent Lefoulon <vincent.lefoulon@free.fr>
2016-12-22 17:32:33 +0100

    Create a Python package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name='javelot',
+    url='https://github.com/Vayel/javelot',
+    author='Vincent Lefoulon',
+    package_dir={'javelot': 'pythonpath'},
+    packages=['javelot'],
+)
